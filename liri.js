d24c8995d12cef34f69495510b1e60b6b3049fbf
Yvonne Reichenbach <yvonnereichenbach@Yvonnes-MacBook-Pro.local>
2018-02-17 21:41:01 -0500

    Update

--- liri.js
+++ liri.js
@@ -1,2 +1,8 @@
 
-require('dotenv').config()
+require('dotenv').config() 
+var validation = require("./key.js");
+var spotify = new Spotify(keys.spotify);
+var client = new Twitter(keys.twitter);
+
+
+
