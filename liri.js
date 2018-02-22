3aa74279afec7365998cc0801ef7b6847906b7c8
Yvonne Reichenbach <yvonnereichenbach@Yvonnes-MacBook-Pro.local>
2018-02-22 01:41:21 -0500

    Twitter

--- liri.js
+++ liri.js
@@ -1,8 +1,25 @@
 
 require('dotenv').config() 
+
 var validation = require("./key.js");
-var spotify = new Spotify(keys.spotify);
-var client = new Twitter(keys.twitter);
 
+var request = require("request");
 
+var Twitter = require('twitter');
+
+var client = new Twitter(validation.twitter);
+
+var params = {
+	q: 'GR3FOU',
+	count: 15
+}
+
+client.get('search/tweets', params, gotData);
+
+function gotData(err, data, response) {
+	var myTweets = data.statuses;
+	for (var i = 0; i < myTweets.length; i++) {
+		console.log(myTweets[i].text);
+	}
+}
 
